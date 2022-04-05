0a2aed832ed2d9bf36b15aa90d1907f9e6596d69
patrickoceandigital <patrickoceandigital@gmail.com>
2022-04-05 15:48:18 -0400

    initial commit

---
+++ src/main/java/whatever/you/wish/backend/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package whatever.you.wish.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+        http.cors(); //disable this line to reproduce the CORS 401
+        http.csrf().disable();
+    }
+
+    @Autowired
+    private Config config;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(config.getCors().getOrigins());
+        //configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        System.out.println(config.getCors().getOrigins());
+        configuration.setAllowedMethods(Arrays.asList("OPTIONS","GET","POST","PUT", "DELETE", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
+
