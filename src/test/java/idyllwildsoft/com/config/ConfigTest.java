11f3b6b73792abd37f58297ad5542b09215530a6
patrickoceandigital <patrickoceandigital@gmail.com>
2022-04-07 15:10:10 -0400

    added tests

---
+++ src/test/java/idyllwildsoft/com/config/ConfigTest.java
@@ -0,0 +1,16 @@
+package idyllwildsoft.com.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConfigTest {
+
+    @Test
+    public void testNothing() {
+        assertTrue(false);
+    }
+
+}
